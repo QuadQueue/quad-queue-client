13f599654789f84894195e669c1542eca7c0c216
razmy <mohamedrazmy@efuturesworld.com>
2022-08-13 21:11:26 +0530

    initial commit

---
+++ src/main/java/lk/quadrate/quadqueueclient/QuadQueueClientApplication.java
@@ -0,0 +1,13 @@
+package lk.quadrate.quadqueueclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadQueueClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadQueueClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/quadrate/quadqueueclient/QuadQueueClientApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.quadrate.quadqueueclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadQueueClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
