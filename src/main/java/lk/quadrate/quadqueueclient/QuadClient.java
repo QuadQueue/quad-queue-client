a59862a9171b8c1f201d9704d101865105556cf9
razmy <mohamedrazmy@efuturesworld.com>
2022-08-14 14:25:52 +0530

    initial commit

--- src/main/java/lk/quadrate/quadqueueclient/QuadClient.java
+++ src/main/java/lk/quadrate/quadqueueclient/QuadClient.java
@@ -3,14 +3,16 @@ package lk.quadrate.quadqueueclient;
 import lk.quadrate.quadqueueclient.configuration.PropertiesConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Slf4j
 @Configuration
 @EnableAutoConfiguration
-@org.springframework.context.annotation.ComponentScan
+@EnableFeignClients(basePackages = "lk.quadrate.quadqueueclient")
+@org.springframework.context.annotation.ComponentScan(basePackages = "lk.quadrate")
 @PropertySource(value = "classpath:quad-client-prop.yml",factory = PropertiesConfig.class)
-public class ComponentScan {
+public class QuadClient {
 
 }
